53566c51032271f08eb15d9ae322cb30da108958
nox-vixen <nox.vixen.0@gmail.com>
2026-06-27 10:48:10 +0530

    Create HomeService skeleton

--- backend/home_service.py
+++ backend/home_service.py
@@ -1,50 +1,19 @@
-from backend.search_service import search_service
 from backend.anilist import get_trending_anime
 
 
 class HomeService:
 
-    def __init__(self):
-        pass
+    def get_home(self):
 
-    def featured(self):
+        featured = get_trending_anime(limit=5)
 
-        featured = []
-
-        trending = get_trending_anime(limit=5)
-
-        for anime in trending:
-
-            results = search_service.search(
-                anime["title"]
-            )
-
-            if not results.items:
-                continue
-
-            moviebox = results.first_item
-
-            featured.append({
-
-                "id": moviebox.subjectId,
-
-                "title": moviebox.title,
-
-                "description": anime["description"],
-
-                "image": anime["bannerImage"],
-
-                "poster": anime["coverImage"],
-
-                "rating": anime["rating"],
-
-                "year": anime["year"],
-
-                "genres": anime["genres"]
-
-            })
-
-        return featured
+        return {
+            "featured": {
+                "lastUpdated": None,
+                "items": featured
+            },
+            "sections": []
+        }
 
 
 home_service = HomeService()
