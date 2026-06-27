1f53c9cac81286bf0cd453119ffaee086546bc01
nox-vixen <nox.vixen.0@gmail.com>
2026-06-27 10:03:03 +0530

    Remove banner blueprint temporarily

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask
 from flask_cors import CORS
 
-from routes.banner import banner_bp
 from routes.anime import anime_bp
 from routes.home import home_bp
 from routes.search import search_bp
@@ -10,7 +9,6 @@ from routes.search import search_bp
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(banner_bp)
 app.register_blueprint(anime_bp)
 app.register_blueprint(home_bp)
 app.register_blueprint(search_bp)
