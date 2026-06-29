20eadaf83945b709097e57106eadfcca61516112
nox-vixen <nox.vixen.0@gmail.com>
2026-06-29 19:45:02 +0530

    Inspect MovieBox Search methods

--- routes/stream.py
+++ routes/stream.py
@@ -5,13 +5,18 @@ stream_bp = Blueprint("stream", __name__)
 @stream_bp.route("/debug/moviebox")
 def debug_moviebox():
 
-    import inspect
-    import moviebox_api.v1 as v1
+    from moviebox_api.v1 import Search
 
     return jsonify({
-        "classes": [
-            name
-            for name, obj in inspect.getmembers(v1)
-            if inspect.isclass(obj)
+
+        "methods": [
+
+            method
+
+            for method in dir(Search)
+
+            if not method.startswith("_")
+
         ]
+
     })
